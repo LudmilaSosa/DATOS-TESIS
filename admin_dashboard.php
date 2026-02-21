74a102a9a7622bf25208aa90ef956716bdf97534
LudmilaSosa <ludmila.belen.vera033@gmail.com>
2026-02-20 22:31:32 -0300

    Add admin_dashboard.php for role-based redirection

---
+++ admin_dashboard.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+
+if (!isset($_SESSION['rol'])) {
+    header("Location: login.php");
+    exit();
+}
+
+switch ($_SESSION['rol']) {
+
+    case "admin":
+        header("Location: DASHBOARD/admin_dashboard.php");
+        break;
+
+    case "empleado":
+        header("Location: DASHBOARD/empleado_dashboard.php");
+        break;
+
+    case "cliente":
+        header("Location: DASHBOARD/cliente_dashboard.php");
+        break;
+
+    default:
+        header("Location: login.php");
+}
+
+exit();
